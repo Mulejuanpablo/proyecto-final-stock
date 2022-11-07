26dd18fc56b1e2ae3be9ef59236ad1b7f8fccb05
mulejuanpablo <mulejuanpablo@gmail.com>
2022-11-07 19:44:54 -0300

    modificado

--- stock/src/main/java/tup/stock/repositories/StockRepository.java
+++ stock/src/main/java/tup/stock/repositories/StockRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import tup.stock.models.Stock;
 
-
+/*contiene la anotación @Repository ; indica que StockRepository extiende CrudRepository y es candidata a la inyección de dependencias  */
  
 @Repository
 public interface StockRepository extends CrudRepository<Stock, Long> {
